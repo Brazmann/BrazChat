e423dd6aa132f99028a7a420509f8d60a0555e7e
Brazman <CrazyC787@GMAIL.COM>
2022-09-10 12:31:14 -0500

    Add project files.

---
+++ BrazChat/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using Grpc;
+using Grpc.Core;
+using Brazchat;
+using Google.Protobuf;
+using Newtonsoft.Json;
+
+namespace BrazChatServer
+{
+    class MessagingImpl : Messaging.MessagingBase
+    {
+
+        public override Task<SendMessageReply> SendMessage(SendMessageRequest request, ServerCallContext context)
+        {
+            string username = request.ClientUsername;
+            if(request.ClientUsername == null)
+            {
+                username = "Guest";
+            }
+            Console.WriteLine($"Message received: '{request.Message}' from {username}");
+            Utilities.LogNewMessage(request.Message, username);
+            return Task.FromResult(new SendMessageReply { Confirmation = "Successfully sent message to target!" });
+        }
+        public override Task<GetMessageLogsReply> GetMessageLogs(GetMessageLogsRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GetMessageLogsReply { Log = File.ReadAllText(@$"{Program.brazChatPath}\log\ChatLog.txt")});
+        }
+        public override Task<GetMessagesReply> GetMessages(GetMessagesRequest request, ServerCallContext context)
+        {
+            List<string> text = File.ReadLines(@$"{Program.brazChatPath}\log\ChatLog.txt").Reverse().Take(20).Reverse().ToList();
+            string messages = string.Join("\n", text);
+            return Task.FromResult(new GetMessagesReply { Confirmation = "Successful!", Messages = messages});
+        }
+        public override Task<WelcomeMessageReply> WelcomeMessage(WelcomeMessageRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new WelcomeMessageReply {WelcomeLogo = Figgle.FiggleFonts.Banner3D.Render("BrazChat") , MOTD = File.ReadAllText(@$"{Program.brazChatPath}\config\MOTD.txt") }); //Make this read the logo from a file in the future.
+        }
+        public override Task<GreetingReply> Greeting(GreetingRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GreetingReply { });
+        }
+
+    }
+    class Program
+    {
+        public static string brazChatPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        const int Port = 50051;
+        public static void Main(string[] args)
+        {
+            CheckNecessaryFiles();
+            List<Config> configItems = new List<Config>();
+            using (StreamReader r = new StreamReader(@$"{Program.brazChatPath}\config\config.json"))
+            {
+                string json = r.ReadToEnd();
+                configItems = JsonConvert.DeserializeObject<List<Config>>(json);
+            }
+            Server server = new Server
+            {
+                Services = { Messaging.BindService(new MessagingImpl()) },
+                Ports = { new ServerPort(configItems[0].Host, configItems[0].Port, ServerCredentials.Insecure)}
+            };
+            server.Start();
+            Console.WriteLine("Server running. Press any key to stop server.");
+            Console.ReadKey();
+        }
+        public static void CheckNecessaryFiles()
+        {
+            if (File.Exists(@$"{Program.brazChatPath}\config\MOTD.txt") == false)
+            {
+                Directory.CreateDirectory($@"{Program.brazChatPath}\config");
+                File.WriteAllText(@$"{Program.brazChatPath}\config\MOTD.txt", "Default MOTD");
+            }
+            if (File.Exists(@$"{Program.brazChatPath}\config\config.json") == false)
+            {
+                Console.WriteLine("No config file found! Exiting.");
+                Environment.Exit(2);
+            }
+            if (File.Exists(@$"{Program.brazChatPath}\log\ChatLog.txt") == false)
+            {
+                Directory.CreateDirectory($@"{Program.brazChatPath}\log");
+                File.WriteAllText(@$"{Program.brazChatPath}\log\ChatLog.txt", "Say something!\n");
+            }
+        }
+    }
+    class Utilities
+    {
+        public static void LogNewMessage(string message, string username)
+        {
+            string path = @$"{Program.brazChatPath}\log\ChatLog.txt";
+
+            File.AppendAllText(path, $"{username}: {message}\n");
+        }
+    }
+
+    public class Config
+    {
+        public string Host;
+        public int Port;
+    }
+}
+
+
---
+++ BrazChatClient/Program.cs
@@ -0,0 +1,213 @@
+﻿using Brazchat;
+using Grpc.Core;
+
+namespace BrazChatClient
+{
+    class Program
+    {
+        public static string username = "";
+        public static string host = "";
+        public static void Main(string[] args)
+        {
+            StartScreen();
+        }
+
+        public static void StartScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter a hostname (ip:port)");
+            host = Console.ReadLine();
+            Console.WriteLine(host);
+            try
+            {
+                Channel channel = new Channel(host, ChannelCredentials.Insecure);
+
+                var client = new Messaging.MessagingClient(channel);
+
+                var reply = client.Greeting(new GreetingRequest { });
+
+                channel.ShutdownAsync().Wait();
+                WelcomeScreen();
+            }
+            catch (RpcException)
+            {
+                Console.WriteLine("Invalid host! Press any key to try again.");
+                Console.ReadKey();
+                Console.Clear();
+                StartScreen();
+            }
+        }
+        public static void WelcomeScreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            string[] WelcomeMessage = GetWelcomeMessage();
+            Console.WriteLine(WelcomeMessage[0]);
+            Console.WriteLine(WelcomeMessage[1]);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Type in a username please!");
+            username = Console.ReadLine();
+            Console.WriteLine("Press any key to start sending messages!");
+            Console.ReadKey();
+            ChatScreen();
+        }
+        public static void ChatScreen()
+        {
+            Console.Clear();
+            while (!Console.KeyAvailable)
+            {
+                Console.CursorVisible = false;
+                CustomClear();
+                Console.SetCursorPosition(0, 0);
+                var Messages = GetMessages();
+                if (Messages.Confirmation == "Nope!")
+                {
+                    ConnectionFailureScreen();
+                }
+                else
+                {
+                    Console.WriteLine($"{Messages.Messages}\n");
+                    //Console.WriteLine("----------------------");
+                    Console.WriteLine("T - Type message \nL - View logs");
+                    Thread.Sleep(100);
+                }
+            }
+                ConsoleKey input = Console.ReadKey(true).Key;
+                switch (input)
+                {
+                    case ConsoleKey.T:
+                        TypeScreen();
+                        break;
+                    case ConsoleKey.L:
+                        ChatLogScreen();
+                        break;
+                    default:
+                        ChatScreen();
+                        break;
+                }
+        }
+
+        public static void TypeScreen()
+        {
+            Console.Clear();
+            var Messages = GetMessages();
+            if (Messages.Confirmation == "Nope!")
+            {
+                ConnectionFailureScreen();
+            }
+            else
+            {
+                Console.WriteLine(Messages.Messages);
+                Console.WriteLine("Type your message!");
+                string? message = Console.ReadLine();
+                SendMessage(message);
+                ChatScreen();
+            }
+        }
+
+        public static void ChatLogScreen()
+        {
+            Console.Clear();
+            var Messages = GetMessages();
+            if(GetMessages().Confirmation == "Nope!")
+            {
+                Console.WriteLine("Connection to server lost! Press any key to return to connection menu!");
+                ConnectionFailureScreen();
+            }else{
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(GetMessageLogs().Log);
+            Console.WriteLine("--------------------------");
+            Console.ReadKey();
+            Console.Clear();
+            ChatScreen();
+            }
+        }
+
+        public static void ConnectionFailureScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("Connection to server lost! Press any key to return to connection menu!");
+            Console.ReadKey();
+            StartScreen();
+        }
+        public static string[] GetWelcomeMessage()
+        {
+            try
+            {
+                Channel channel = new Channel(host, ChannelCredentials.Insecure);
+
+                var client = new Messaging.MessagingClient(channel);
+
+                var reply = client.WelcomeMessage(new WelcomeMessageRequest { });
+
+                string[] WelcomeMessage = { reply.WelcomeLogo, reply.MOTD };
+
+                channel.ShutdownAsync().Wait();
+                return WelcomeMessage;
+            }
+            catch (RpcException)
+            {
+                string[] Fallback = { "Brazchat", "ERRORNOCONNECTION" }; //0 = Logo, 1 = MOTD
+                return Fallback;
+            }
+        }
+        public static void SendMessage(string? message)
+        {
+            try
+            {
+                Channel channel = new Channel(host, ChannelCredentials.Insecure);
+
+                var client = new Messaging.MessagingClient(channel);
+
+                var reply = client.SendMessage(new SendMessageRequest { ClientUsername = username, Message = message, Color = "Red" });
+                Console.WriteLine("Message sent. Result:" + reply.Confirmation);
+
+                channel.ShutdownAsync().Wait();
+            }
+            catch (Grpc.Core.RpcException)
+            {
+                ConnectionFailureScreen();
+            }
+        }
+
+        public static GetMessagesReply GetMessages()
+        {
+            try
+            {
+                Channel channel = new Channel(host, ChannelCredentials.Insecure);
+
+                var client = new Messaging.MessagingClient(channel);
+                String clientUsername = "Brazman";
+
+                var reply = client.GetMessages(new GetMessagesRequest { ClientUsername = clientUsername });
+                channel.ShutdownAsync().Wait();
+                return reply;
+            }
+            catch (Grpc.Core.RpcException)
+            {
+                //lord forgive me
+                var Fallback = new GetMessagesReply { Confirmation = "Nope!", Messages = "No connection! Retrying!\n" };
+                return Fallback;
+            }
+        }
+        public static GetMessageLogsReply GetMessageLogs()
+        {
+            Channel channel = new Channel(host, ChannelCredentials.Insecure);
+
+            var client = new Messaging.MessagingClient(channel);
+
+            var reply = client.GetMessageLogs(new GetMessageLogsRequest { });
+            channel.ShutdownAsync().Wait();
+            return reply;
+        }
+
+        public static void CustomClear()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int y = 0; y < Console.WindowHeight; y++)
+                Console.Write(new String(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, 0);
+        }
+
+    }
+}
